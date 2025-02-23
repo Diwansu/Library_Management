c7c83ec234651d68dd9db97449dd9234eba699e1
Diwanshu Baskota <diwanshubaskota@Diwanshus-MacBook-Air-341.local>
2025-02-23 20:32:44 +0530

    Tested the endpoints of Member Entity By Postman

--- backend/config/database.js
+++ backend/config/database.js
@@ -24,7 +24,7 @@ const connectDB = async () => {
     console.log("✅ Connected to Database");
   } catch (err) {
     console.error("❌ Database connection error:", err);
-    process.exit(1); 
+    // process.exit(1); 
   }
 };
 
